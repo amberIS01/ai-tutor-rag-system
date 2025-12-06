b3a91d7772443146e5a4fb30cd90b51d94bebdec
Sahil Singh <sahilsingh8300@gmail.com>
2025-12-06 21:05:22 +0530

    feat: add middleware for request tracking and performance monitoring

---
+++ backend/middleware.py
@@ -0,0 +1,38 @@
+"""
+Custom Middleware
+Request/Response middleware for the application
+"""
+
+from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.responses import Response
+import time
+import uuid
+
+
+class RequestIDMiddleware(BaseHTTPMiddleware):
+    """Add unique request ID to each request"""
+    
+    async def dispatch(self, request: Request, call_next):
+        request_id = str(uuid.uuid4())
+        request.state.request_id = request_id
+        
+        response = await call_next(request)
+        response.headers["X-Request-ID"] = request_id
+        
+        return response
+
+
+class TimingMiddleware(BaseHTTPMiddleware):
+    """Add request timing header"""
+    
+    async def dispatch(self, request: Request, call_next):
+        start_time = time.time()
+        
+        response = await call_next(request)
+        
+        process_time = time.time() - start_time
+        response.headers["X-Process-Time"] = str(process_time)
+        
+        return response
+
